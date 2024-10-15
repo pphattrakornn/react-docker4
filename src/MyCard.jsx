5798894da7d82f776fe96b624e829574f9291e86
pphattrakornn <phattrakorn.c@ku.th>
2024-10-15 21:38:36 +0700

    edit 02

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -26,7 +26,7 @@ export default function MyCard(props) {
     </Typography>
   </CardContent>
   <CardActions>
-    <Button size="small">xxx</Button>
+    <Button size="small">xxx2</Button>
     <Button size="small">Learn More</Button>
   </CardActions>
 </Card>
